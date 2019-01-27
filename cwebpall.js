bfa0b6e5b40e1df2bea38d9d69fd06cca488e31c
agamemnus <agamemnus@users.noreply.github.com>
2019-01-26 19:18:40 -0500

    Update cwebpall.js

--- cwebpall.js
+++ cwebpall.js
@@ -5,12 +5,13 @@ let exec = require('child_process').exec
 
 var pattern = process.argv[2]
 var percent = process.argv[3]
+var outputDirectory = process.argv[3]
 if (!percent) percent = 90
-
+if (!outputDirectory) outputDirectory = ""
 var options = {}
 glob("./" + pattern, options, function (er, files) {
  files.forEach(function (input) {
   let output = input.substring(0, input.lastIndexOf('.')) + '.webp'
-  exec('cwebp -quiet -q ' + percent + ' ' + input + ' -o ' + output)
+  exec('cwebp -quiet -q ' + percent + ' ' + input + ' -o ' + outputDirectory + output)
  })
 })
